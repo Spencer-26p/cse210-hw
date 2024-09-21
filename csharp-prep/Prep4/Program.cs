d7131cce0e59c6dd2ab29d3ab782a5cfd66615b6
Spencer-26p <spencer1997@byui.edu>
2024-09-21 14:43:30 -0500

    Finished Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int response = -100;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (response != 0)
+        {
+            Console.Write("Enter number: ");
+            string responseString = Console.ReadLine();
+            response = int.Parse(responseString);
+            if(response != 0)
+            {
+            numbers.Add(response);
+            }
+        }
+        float sum = 0;
+        float average = 0;
+        int large = 0;
+        int small = 10000;
+        numbers.Sort();
+        foreach (int number in numbers)
+        {
+            if (number > 0 && number < small)
+            {
+                small = number;
+            }
+            sum =+ number;
+        }
+        average = sum / numbers.Count;
+        large = numbers[numbers.Count - 1];
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest is: {large}");
+        Console.WriteLine($"The smallest positive number: {small}");
+        Console.WriteLine("The sorted list is:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine($"{number}");
+        }
     }
 }
