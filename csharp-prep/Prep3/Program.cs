597ceac8d7522fa7bcb6b393437cecada6fccc3e
Spencer-26p <spencer1997@byui.edu>
2024-09-21 14:10:17 -0500

    Finished Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "yes";
+        while (playAgain == "yes" || playAgain == "y")
+        {
+            int guessCount = 0;
+            Random randomGenerator = new Random();
+            int randomNum = randomGenerator.Next(1,100);
+            int guess = -1000;
+            while (guess != randomNum)
+            {
+                Console.WriteLine("What is your guess? ");
+                string guessString = Console.ReadLine();
+                guessCount +=1;
+                guess = int.Parse(guessString);
+                if (guess == randomNum)
+                {
+                    Console.WriteLine($"Congratulations! It took you {guessCount} tries! Would you like to try again? (yes or no)? ");
+                    playAgain = Console.ReadLine();
+                }
+                else if (guess < randomNum)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > randomNum)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("I don't understand, try again.");
+                    guessCount =-1;
+                }
+            }
+        }
     }
 }
